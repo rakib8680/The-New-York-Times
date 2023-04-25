2f47d075dc98994720d2c44c1327b97de5de67cf
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-25 20:30:24 +0600

    header part done

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import React from 'react';
 import Marquee from 'react-fast-marquee';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png'
 const Header = () => {
     return (
@@ -10,13 +11,43 @@ const Header = () => {
                 <p className='text-secondary'>Journalism Without Fear or Favour</p>
                 <p className='text-warning'>{moment().format("dddd, MMMM Do, YYYY")}</p>
             </div>
-            <div className='flex bg-neutral py-4 ps-4 pe-24 gap-4 '>
+            <div className='flex bg-neutral py-4 ps-4 pe-24 gap-4 rounded-md'>
                 <button className='btn btn-error btn-sm px-4 pt-3 pb-6'>Latest</button>
-                <Marquee className='font-semibold text-lg text-accent' pauseOnHover speed={80} gradient={true} gradientColor={[30, 40, 46]}>
+                <Marquee className='font-semibold text-lg text-accent' pauseOnHover speed={80}  gradientColor={[30, 40, 46]}>
                     Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as.....
                     Match Highlights: Germany vs Spain — as it happened   !
                 </Marquee>
             </div>
+            <nav>
+                <nav className="navbar bg-base shadow-lg  my-5">
+                    <div className="container mx-auto ">
+
+                        <ul className="navbar-nav flex mx-auto">
+                            <li className="nav-item">
+                                <NavLink  to="/home" className="nav-link text-secondary mx-4 hover:text-indigo-500">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink  to="/about" className="nav-link text-secondary mx-4 hover:text-indigo-500">
+                                    About
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink  to="/career" className="nav-link text-secondary mx-4 hover:text-indigo-500">
+                                    Career
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <div className=' flex items-center rounded-md'>
+                            <p className='text-secondary mx-4'>Profile</p>
+                            <button className="btn  btn-info ">
+                                Login
+                            </button>
+                        </div>
+                    </div>
+                </nav>
+            </nav>
         </div>
     );
 };
