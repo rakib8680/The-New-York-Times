536f5c4e94e125591a1c41062de72eb4d9d2c424
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-25 19:34:22 +0600

    marquee added

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import React from 'react';
+import Marquee from 'react-fast-marquee';
 import logo from '../assets/logo.png'
 const Header = () => {
     return (
@@ -9,8 +10,12 @@ const Header = () => {
                 <p className='text-secondary'>Journalism Without Fear or Favour</p>
                 <p className='text-warning'>{moment().format("dddd, MMMM Do, YYYY")}</p>
             </div>
-            <div>
+            <div className='flex bg-neutral py-4 ps-4 pe-24 gap-4 '>
                 <button className='btn btn-error btn-sm px-4 pt-3 pb-6'>Latest</button>
+                <Marquee className='font-semibold text-lg text-accent' pauseOnHover speed={80} gradient={true} gradientColor={[30, 40, 46]}>
+                    Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as.....
+                    Match Highlights: Germany vs Spain — as it happened   !
+                </Marquee>
             </div>
         </div>
     );
