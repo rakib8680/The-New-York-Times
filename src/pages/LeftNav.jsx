c68b57ef05f643834c9e11c34f27275a633643d1
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-26 13:45:22 +0600

    server created and data loaded

--- src/pages/LeftNav.jsx
+++ src/pages/LeftNav.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(categories => console.log(categories))
+        .catch(err => console.error(err))
+    }, [])
+
+
+
+
     return (
         <div>
             Left Nav
